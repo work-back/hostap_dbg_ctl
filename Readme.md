4f05cf3ed1452eb45b0945360fb7fff89f07b233
langyanjun <langyanjun@formovie.cn>
2023-06-30 10:44:33 +0800

    up

--- Readme.md
+++ Readme.md
@@ -1,2 +1,3 @@
-#test build 
+# test build 
+
 gcc hostapd/dbg_test.c hostapd/dbg_ctl.c -I src/ -D DBG_CTL_TEST -Wall -o dbg_ctl
--- hostapd/dbg_test.c
+++ hostapd/dbg_test.c
@@ -3,6 +3,18 @@
 
 int main(void)
 {
+    char cmd[1024] = {0};
+    char buf[1024] = {0};
+
     dbg_ctl_init();
+
+    snprintf(cmd, sizeof(cmd), "%s", "pmkid_lost ADD 11:22:33:44:55:66");
+    dbg_ctl_run(cmd, buf, sizeof(buf));
+    wpa_printf(MSG_ERROR, "RESP ====> %s", buf);
+
+    snprintf(cmd, sizeof(cmd), "%s", "pmkid_lost");
+    dbg_ctl_run(cmd, buf, sizeof(buf));
+    wpa_printf(MSG_ERROR, "RESP ====> %s", buf);
+
     return 0;
 }
